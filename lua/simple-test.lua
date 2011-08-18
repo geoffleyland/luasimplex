ab529eb8d273463863a559a3d1323a1349a726d7
Geoff Leyland <geoff.leyland@incremental.co.nz>
2011-08-18 19:10:09 +1200

    simple-test.lua uses new iarray and darray

--- lua/simple-test.lua
+++ lua/simple-test.lua
@@ -1,3 +1,4 @@
+local luasimplex = require("luasimplex")
 local rsm = require("luasimplex.rsm")
 
 local M =
@@ -10,19 +11,19 @@ local M =
   {
     {
       elements = 3,
-      indexes = rsm.iarray(3, 1, 2, 3),
-      values = rsm.darray(3, 1, 2, 1),
+      indexes = luasimplex.iarray(3, 1, 2, 3),
+      values = luasimplex.darray(3, 1, 2, 1),
     },
     {
       elements = 3,
-      indexes = rsm.iarray(3, 1, 2, 4),
-      values = rsm.darray(3, 2, 1, 1),
+      indexes = luasimplex.iarray(3, 1, 2, 4),
+      values = luasimplex.darray(3, 2, 1, 1),
     }
   },
-  c = rsm.darray(4, -1, -1, 0, 0),
-  xl = rsm.darray(4, 0, 0, 0, 0),
-  xu = rsm.darray(4, math.huge, math.huge, math.huge, math.huge),
-  b = rsm.darray(2, 3, 3),
+  c = luasimplex.darray(4, -1, -1, 0, 0),
+  xl = luasimplex.darray(4, 0, 0, 0, 0),
+  xu = luasimplex.darray(4, math.huge, math.huge, math.huge, math.huge),
+  b = luasimplex.darray(2, 3, 3),
 }
 
 objective, x = rsm.solve(M, {})
